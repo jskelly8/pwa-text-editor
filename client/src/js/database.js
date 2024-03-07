71b2566f2eb78e800ff6ac218cf4d510a1a52b8d
Jess Skelton <jessskelton888@gmail.com>
2024-03-06 22:11:47 -0600

    Completed putDb and getDb in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,23 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Added logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const result = await store.put({ id: 1, value: content }); // Assuming there's only one entry
+  console.log('Data saved to the DB', result);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Added logic for a method that gets all the content from the database
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const result = await store.getAll();
+  console.log('Data fetched from the DB', result);
+  return result?.value;
+}
 
 initdb();
