5a4a814133ee1177772ee5333d3064602c349f58
Jess Skelton <jessskelton888@gmail.com>
2024-03-06 21:21:41 -0600

    Added .gitignore --- Updated main package.json --- Began sw config with plugins config on ./client/src-sw.js

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,12 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      // Plugin configuration -------- add later (Workbox)
+    ],
+  })
+);
